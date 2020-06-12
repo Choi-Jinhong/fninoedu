4dc60428f1ab10cd7a0ff3e4a0c10390f60c874a
Choi_Jinhong <chlwlsghd58@gmail.com>
2020-06-12 09:11:20 +0900

    자바스크립트 관련

---
+++ JSP/web/Day0611/popup.js
@@ -0,0 +1,7 @@
+function pop1() {
+    window.open('img/Image/popup3.jpg','Event','width=430, height=430, scrollbar=yes, status=no');
+}
+
+function pop2() {
+    window.open('http://www.naver.com','Naver','width=430, height=430, scrollbar=yes, status=no');
+}
